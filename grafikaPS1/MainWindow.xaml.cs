e86576d39114eff0d86eadc71b3c5fafa0fa588f
ewakors <ewakorszaczuk@vp.pl>
2017-10-08 22:35:38 +0200

    add adorners

--- grafikaPS1/MainWindow.xaml.cs
+++ grafikaPS1/MainWindow.xaml.cs
@@ -20,8 +20,15 @@ namespace grafikaPS1
     /// </summary>
     public partial class MainWindow : Window
     {
- 
+        bool isDown, isDragging, isSelected;
+        UIElement selectedElement = null;
+        double originalLeft, originalTop;
+        Point startPoint;
+
+        AdornerLayer adornerLayer;
+
         public Rectangle change { get; set; }
+
         private enum SelectedShape
         { None, Line, Ellipse, Rectangle }
 
@@ -29,8 +36,114 @@ namespace grafikaPS1
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
         }
 
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //registering mouse events
+            this.MouseLeftButtonDown += MainWindow_MouseLeftButtonDown;
+            this.MouseLeftButtonUp += MainWindow_MouseLeftButtonUp;
+            this.MouseMove += MainWindow_MouseMove;
+            this.MouseLeave += MainWindow_MouseLeave;
+
+            canvasArea.PreviewMouseLeftButtonDown += MyCanvas_PreviewMouseLeftButtonDown;
+            canvasArea.PreviewMouseLeftButtonUp += MyCanvas_PreviewMouseLeftButtonUp;
+        }
+
+        private void StopDragging()
+        {
+            if (isDown)
+            {
+                isDown = isDragging = false;
+            }
+        }
+
+
+        private void MyCanvas_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            StopDragging();
+            e.Handled = true;
+        }
+
+        private void MyCanvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //removing selected element
+            if (isSelected)
+            {
+                isSelected = false;
+                if (selectedElement != null)
+                {
+                    adornerLayer.Remove(adornerLayer.GetAdorners(selectedElement)[0]);
+                    selectedElement = null;
+                }
+            }
+
+            // select element if any element is clicked other then canvas
+            if (e.Source != canvasArea)
+            {
+                isDown = true;
+                startPoint = e.GetPosition(canvasArea);
+
+                selectedElement = e.Source as UIElement;
+
+                originalLeft = Canvas.GetLeft(selectedElement);
+                originalTop = Canvas.GetTop(selectedElement);
+
+                //adding adorner on selected element
+                adornerLayer = AdornerLayer.GetAdornerLayer(selectedElement);
+                adornerLayer.Add(new SimpleCircleAdorner(selectedElement));
+                isSelected = true;
+                e.Handled = true;
+            }
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            //handling mouse move event and setting canvas top and left value based on mouse movement
+            if (isDown)
+            {
+                if ((!isDragging) &&
+                    ((Math.Abs(e.GetPosition(canvasArea).X - startPoint.X) > SystemParameters.MinimumHorizontalDragDistance) ||
+                    (Math.Abs(e.GetPosition(canvasArea).Y - startPoint.Y) > SystemParameters.MinimumVerticalDragDistance)))
+                    isDragging = true;
+
+                if (isDragging)
+                {
+                    Point position = Mouse.GetPosition(canvasArea);
+                    Canvas.SetTop(selectedElement, position.Y - (startPoint.Y - originalTop));
+                    Canvas.SetLeft(selectedElement, position.X - (startPoint.X - originalLeft));
+                }
+            }
+        }
+
+        private void MainWindow_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //stop dragging on mouse leave
+            StopDragging();
+            e.Handled = true;
+        }
+
+        private void MainWindow_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //stop dragging on mouse left button up
+            StopDragging();
+            e.Handled = true;
+        }
+
+        private void MainWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //remove selected element on mouse down
+            if (isSelected)
+            {
+                isSelected = false;
+                if (selectedElement != null)
+                {
+                    adornerLayer.Remove(adornerLayer.GetAdorners(selectedElement)[0]);
+                    selectedElement = null;
+                }
+            }
+        }
 
         private void addEllipse_Click(object sender, RoutedEventArgs e)
         {
